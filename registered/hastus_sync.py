52085b7dfe5d4179a0accecb9880c220c97ffba3
ayan-mbta <ayan@mbta.com>
2026-08-20 14:15:39 -0400

    cleaned up network config, tested on both v17 and v26

--- registered/hastus_sync.py
+++ registered/hastus_sync.py
@@ -20,6 +20,7 @@ from registered import calendar, cheat_sheet, environ, merge, parser, seasons, v
 HASTUS = environ["HASTUS_FILE_SERVER"]
 TRANSITMASTER = environ["TRANSITMASTER_FILE_SERVER"]
 TRANSITMASTER_DB = environ["TRANSITMASTER_DATABASE_SERVER"]
+TRANSITMASTER_FTP = environ ["TRANSITMASTER_FTP_SERVER"]
 SLASH = "\\"
 
 
@@ -182,8 +183,8 @@ def pull_prior_versions(tempdir):
     """
     smbclient.shutil.copyfile(
         smb_path(
-            TRANSITMASTER_DB,
-            "e$",
+            TRANSITMASTER_FTP,
+            "d$",
             "FTP_ROOT",
             "Operational Data",
             "Route Data",
@@ -194,8 +195,8 @@ def pull_prior_versions(tempdir):
         tempdir / "PriorVersions" / "svc-desc.txt",
     )
     annun_path = smb_path(
-        TRANSITMASTER_DB,
-        "e$",
+        TRANSITMASTER_FTP,
+        "d$",
         "FTP_ROOT",
         "Operational Data",
         "Announcements",
@@ -213,8 +214,8 @@ def pull_prior_versions(tempdir):
         )
     smbclient.shutil.copyfile(
         smb_path(
-            TRANSITMASTER_DB,
-            "e$",
+            TRANSITMASTER_FTP,
+            "d$",
             "FTP_ROOT",
             "Operational Data",
             "Announcements",
@@ -270,7 +271,7 @@ def push_directory(args, tempdir):
         for dirname in dirnames:
             dst = smb_path(
                 TRANSITMASTER,
-                "C$",
+                "D$",
                 "Ratings",
                 args.rating_folder,
                 short_path,
@@ -283,7 +284,7 @@ def push_directory(args, tempdir):
                 continue
             src = Path(dirpath) / filename
             dst = smb_path(
-                TRANSITMASTER, "C$", "Ratings", args.rating_folder, short_path, filename
+                TRANSITMASTER, "D$", "Ratings", args.rating_folder, short_path, filename
             )
             print(f"Pushing {dst}...")
             smbclient.shutil.copy(str(src), dst)
